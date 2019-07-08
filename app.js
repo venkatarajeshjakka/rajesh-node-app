bb5c75452597b327a66182343e5823620ce169b2
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2019-07-08 18:58:33 -0400

    changed to yahoo finance

--- app.js
+++ app.js
@@ -1,80 +1,26 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser =require('body-parser');
-var API = require('indian-stock-exchange');
-const NSE = require('sharewatch').NSE;
-const BSE = require('sharewatch').BSE;
+
 var app = new express();
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended : false}));
-
-var NSEAPI = API.NSE;
-
+var yahooFinance = require('yahoo-finance');
 
 app.get('/api/get-nse-stocks',function(req, res) {
-    NSE.equityList()
-    .then((result) => {
-        
-        res.json(result);
-    })
-    .catch((err) => {
-        // handle error
-    })
+    const quoteName = req.query.stockCode || 'IBULHSGFIN.NS';
+
+    var stockCodeArray = quoteName.split(',');
+
+    yahooFinance.quote({
+        symbols: stockCodeArray,
+        modules: [ 'price', 'summaryDetail' ] // see the docs for the full list
+      }, function (err, quotes) {
+        res.json(quotes);
+        // ...
+      });  
 } );
-
-//To get quote info with stock code
-app.get('/api/get-quote-info',function(req,res)
-{
-    const quoteName = req.query.stockCode || 'IBULHSGFIN';
-     NSE.quote(quoteName).then((result) =>
-     {
-         res.json(result);
-     }).catch((err) =>
-     {
-         console.log('some error in fetching');
-         res.json(err);
-     });
-
-});
-
-app.get('/api/get-peer-comparison',function(req,res)
-{
-    BSE.quoteWithComparison('500209').then((result) =>
-     {
-         res.json(result);
-     }).catch((err) =>
-     {
-         console.log('some error in fetching');
-         res.json(err);
-     });
-
-});
-
-app.get('/api/nse-indicies',function(req,res)
-{
-    NSE.indices().then((result) =>
-     {
-         res.json(result);
-     }).catch((err) =>
-     {
-         console.log('some error in fetching');
-     });
-
-});
-
-app.get('/api/get52WeekHigh',function(req,res)
-{
-    NSEAPI.get52WeekHigh()
-    .then(function (response) { 
-   
-   res.json(response.data);
-    }).catch(err =>
-        {
-            res.json(err);
-        });
-
-});
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
