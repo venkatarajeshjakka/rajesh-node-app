4f93fcecaa58cb467f099a5ea99ccadd34c06ea9
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2020-04-05 20:51:48 -0400

    added x-requested

--- src/nse/config.js
+++ src/nse/config.js
@@ -22,7 +22,9 @@ const headers = {
   "User-Agent":
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
   Connection: "keep-alive",
-  Referer: "https://www.nseindia.com"
+  Referer: "https://www.nseindia.com",
+  "x-requested-with": "XMLHttpRequest"
+
 };
 
 module.exports = {
