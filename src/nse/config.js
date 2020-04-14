6dd7a817547a6467287dc250dfaf38f4b535ee97
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2020-04-14 14:37:09 -0400

    cleared headers

--- src/nse/config.js
+++ src/nse/config.js
@@ -15,20 +15,10 @@ const year_low_url = `${product}/online52NewLow.json`;
 const preopen_nifty_url = `${liveMarket}/live_analysis/pre_open/nifty.json`;
 
 const headers = {
-  Accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-  "Accept-Encoding": "gzip, deflate, br",
-  "Accept-Language": "en-US,en;q=0.9",
-  "Cache-Control": "max-age=0",
-  Connection: "keep-alive",
+  Accept: "*/*",
   Host: "www1.nseindia.com",
   Referer: "https://www.nseindia.com",
   "x-requested-with": "XMLHttpRequest",
-  "Sec-Fetch-Dest": 'document',
-  "Sec-Fetch-Mode": "navigate",
-  "Sec-Fetch-Site": "none",
-  "Sec-Fetch-User": "?1",
-  "Upgrade-Insecure-Requests": 1,
   "User-Agent":
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
 };
